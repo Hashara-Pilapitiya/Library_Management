4991d1c7533426107bf75fe64c9376f9b6be3a80
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-08-10 17:28:18 +0530

    Add LibraryContext class for backend data storage

---
+++ backend/Data/LibraryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
